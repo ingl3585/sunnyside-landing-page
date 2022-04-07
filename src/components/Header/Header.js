1648633e02bbf2d7e22e763ec8b245b597c1f56c
Tony Ingle <ingleanthonyco@gmail.com>
2022-04-07 12:56:41 -0600

    implemented mobile nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
-import Hamburger from '../../images/icon-hamburger.svg';
 import Logo from '../../images/logo.svg';
 
 const Header = () => {
+	const [activeMenu, setActiveMenu] = useState('false');
+	const [activeNav, setActiveNav] = useState('false');
+	const [activeMenuBg, setActiveMenuBg] = useState('false');
+	const onClickMenu = () => {
+		setActiveMenu(!activeMenu);
+		setActiveNav(!activeNav);
+		setActiveMenuBg(!activeMenuBg);
+	};
 	return (
 		<header>
 			<nav>
-				<img src={Logo} alt='sunnyside' />
-				<img src={Hamburger} alt='hamburger-nav' />
-				<ul className='nav-list'>
-					<li className='nav-item'>
-						<a href='#'>About</a>
-					</li>
-					<li className='nav-item'>
-						<a href='#'>Services</a>
-					</li>
-					<li className='nav-item'>
-						<a href='#'>Projects</a>
-					</li>
-					<li className='nav-item'>
-						<a href='#'>Contact</a>
-					</li>
-				</ul>
+				<img src={Logo} alt='sunny-side' />
+				<div className='menu-bar'>
+					<div
+						className={`menu ${activeMenu ? 'change' : ''}`}
+						onClick={onClickMenu}>
+						<div className='menu-bar-1 bar'></div>
+						<div className='menu-bar-2 bar'></div>
+						<div className='menu-bar-3 bar'></div>
+					</div>
+					<ul className={`nav-list ${activeNav ? 'change' : ''}`}>
+						<li className='nav-item'>
+							<a href='#'>About</a>
+						</li>
+						<li className='nav-item'>
+							<a href='#'>Services</a>
+						</li>
+						<li className='nav-item'>
+							<a href='#'>Projects</a>
+						</li>
+						<li className='nav-item'>
+							<a href='#'>Contact</a>
+						</li>
+					</ul>
+				</div>
+				<div className={activeMenuBg ? 'change-bg' : 'menu-bg'}></div>
 			</nav>
-			{/* <section>
+			<section>
 				<h1>We are creatives</h1>
-			</section> */}
+			</section>
 		</header>
 	);
 };
