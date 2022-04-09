b27cdba43d0ad02e5960f9c3649edda169a6e639
Tony Ingle <ingleanthonyco@gmail.com>
2022-04-09 13:18:40 -0600

    footer (mobile) completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import Logo from '../../images/logo.svg';
+import Facebook from '../../images/icon-facebook.svg';
+import Instagram from '../../images/icon-instagram.svg';
+import Twitter from '../../images/icon-twitter.svg';
+import Pinterest from '../../images/icon-pinterest.svg';
+import './footer.css';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<img className='footer-logo' src={Logo} alt='footer-logo' />
+			<ul className='footer-links'>
+				<li>
+					<a href='#'>About</a>
+				</li>
+				<li>
+					<a href='#'>Services</a>
+				</li>
+				<li>
+					<a href='#'>Projects</a>
+				</li>
+			</ul>
+			<ul className='social-links'>
+				<li>
+					<img src={Facebook} alt='social' />
+				</li>
+				<li>
+					<img src={Instagram} alt='social' />
+				</li>
+				<li>
+					<img src={Twitter} alt='social' />
+				</li>
+				<li>
+					<img src={Pinterest} alt='social' />
+				</li>
+			</ul>
+		</footer>
+	);
 };
 
 export default Footer;
